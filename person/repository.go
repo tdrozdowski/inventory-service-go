bc6b19bcb145450ba717e5179baf95dd1f80dea0
terry <terry@xymox.dev>
2024-09-01 19:30:35 -0700

    Add unit tests and implementations for PersonRepository
    Implemented unit tests for GetById, GetByUuid, Create, Update, and DeleteByUuid methods in PersonRepository. Enhanced repository methods to interact with the database using sqlx and sqlmock for testing purposes.

--- person/repository.go
+++ person/repository.go
@@ -39,7 +39,6 @@ type PersonRepository interface {
 	GetByUuid(uuid uuid.UUID) (PersonRow, error)
 	Create(request CreatePersonRequest) (PersonRow, error)
 	Update(request UpdatePersonRequest) (PersonRow, error)
-	Delete(id int) (commons.DeleteResult, error)
 	DeleteByUuid(uuid uuid.UUID) (commons.DeleteResult, error)
 }
 
@@ -71,25 +70,55 @@ func (p *PersonRepositoryImpl) GetAll(pagination *commons.Pagination) ([]PersonR
 }
 
 func (p *PersonRepositoryImpl) GetById(id int) (PersonRow, error) {
-	panic("implement me")
+	// uses sqlx to query the persons table and retrieve a single row by id
+	var person PersonRow
+	err := p.db.Get(&person, "SELECT * FROM persons WHERE id = ?", id)
+	if err != nil {
+		return PersonRow{}, err
+	}
+	return person, nil
 }
 
 func (p *PersonRepositoryImpl) GetByUuid(uuid uuid.UUID) (PersonRow, error) {
-	panic("implement me")
+	// uses sqlx to query the persons table and retrieve a single row by uuid
+	var person PersonRow
+	err := p.db.Get(&person, "SELECT * FROM persons WHERE alt_id = ?", uuid)
+	if err != nil {
+		return PersonRow{}, err
+	}
+	return person, nil
 }
 
 func (p *PersonRepositoryImpl) Create(request CreatePersonRequest) (PersonRow, error) {
-	panic("implement me")
+	// uses sqlx to insert a new row into the persons table
+	var person PersonRow
+	err := p.db.Get(&person, "INSERT INTO persons (name, email, created_by) VALUES (?, ?, ?) RETURNING *", request.Name, request.Email, request.CreatedBy)
+	if err != nil {
+		return PersonRow{}, err
+	}
+	return person, nil
 }
 
 func (p *PersonRepositoryImpl) Update(request UpdatePersonRequest) (PersonRow, error) {
-	panic("implement me")
-}
-
-func (p *PersonRepositoryImpl) Delete(id int) (commons.DeleteResult, error) {
-	panic("implement me")
+	// uses sqlx to update a row in the persons table
+	var person PersonRow
+	err := p.db.Get(&person, "UPDATE persons SET name = ?, email = ?, last_change_by = ? WHERE alt_id = ? RETURNING *", request.Name, request.Email, request.LastChangeBy, request.Id)
+	if err != nil {
+		return PersonRow{}, err
+	}
+	return person, nil
 }
 
 func (p *PersonRepositoryImpl) DeleteByUuid(uuid uuid.UUID) (commons.DeleteResult, error) {
-	panic("implement me")
+	// uses sqlx to delete a row from the persons table by uuid
+	sqlResults := p.db.MustExec("DELETE FROM persons WHERE alt_id = ?", uuid)
+	rowsAffected, err := sqlResults.RowsAffected()
+	if err != nil {
+		return commons.DeleteResult{}, err
+	}
+	result := commons.DeleteResult{
+		Id:      uuid,
+		Deleted: rowsAffected > 0,
+	}
+	return result, nil
 }
--- person/repository_test.go
+++ person/repository_test.go
@@ -1,8 +1,10 @@
 package person
 
 import (
+	"database/sql"
 	"errors"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"inventory-service-go/commons"
 	"testing"
@@ -103,6 +105,374 @@ func TestPersonRepositoryImpl_GetAll(t *testing.T) {
 				t.Errorf("PersonRepositoryImpl.GetAll() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+		})
+	}
+}
+
+func TestPersonRepositoryImpl_GetById(t *testing.T) {
+	type fields struct {
+		db *sqlx.DB
+	}
+	type args struct {
+		id int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		prepare func(mock sqlmock.Sqlmock)
+	}{
+		{
+			name: "Success",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				id: 1,
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id", "name"}).
+					AddRow(1, "test name")
+				mock.ExpectQuery("SELECT \\* FROM persons WHERE id = \\?").
+					WithArgs(1).WillReturnRows(rows)
+			},
+			wantErr: false,
+		},
+		{
+			name: "DB error",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				id: 1,
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("SELECT \\* FROM persons WHERE id = \\?").
+					WithArgs(1).WillReturnError(errors.New("test error"))
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock, _ := sqlmock.New()
+			defer func(db *sql.DB) {
+				err := db.Close()
+				if err != nil {
+
+				}
+			}(db)
+			tt.prepare(mock)
+			tt.fields.db = sqlx.NewDb(db, "sqlmock")
+			p := &PersonRepositoryImpl{
+				db: tt.fields.db,
+			}
+			_, err := p.GetById(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PersonRepositoryImpl.GetById() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestPersonRepositoryImpl_GetByUuid(t *testing.T) {
+	type fields struct {
+		db *sqlx.DB
+	}
+	type args struct {
+		uuid uuid.UUID
+	}
+	testUuid, _ := uuid.Parse("2b1b425e-dee2-4227-8d94-f470a0ce0cd0")
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		prepare func(mock sqlmock.Sqlmock)
+	}{
+		{
+			name: "Success",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				uuid: testUuid,
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id", "name"}).
+					AddRow(1, "test name")
+				mock.ExpectQuery("SELECT \\* FROM persons WHERE alt_id = \\?").
+					WithArgs("2b1b425e-dee2-4227-8d94-f470a0ce0cd0").WillReturnRows(rows)
+			},
+			wantErr: false,
+		},
+		{
+			name: "DB error",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				uuid: testUuid,
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("SELECT \\* FROM persons WHERE alt_id = \\?").
+					WithArgs("2b1b425e-dee2-4227-8d94-f470a0ce0cd0").WillReturnError(errors.New("test error"))
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock, _ := sqlmock.New()
+			defer func(db *sql.DB) {
+				_ = db.Close()
+			}(db)
+			tt.prepare(mock)
+			tt.fields.db = sqlx.NewDb(db, "sqlmock")
+			p := &PersonRepositoryImpl{
+				db: tt.fields.db,
+			}
+			_, err := p.GetByUuid(tt.args.uuid)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PersonRepositoryImpl.GetByUuid() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestPersonRepositoryImpl_Create(t *testing.T) {
+	type fields struct {
+		db *sqlx.DB
+	}
+	type args struct {
+		request CreatePersonRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		prepare func(mock sqlmock.Sqlmock)
+	}{
+		{
+			name: "Success",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				request: CreatePersonRequest{
+					Name:      "test name",
+					Email:     "test email",
+					CreatedBy: "test user",
+				},
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id", "name"}).
+					AddRow(1, "test name")
+				mock.ExpectQuery("INSERT INTO persons ").
+					WillReturnRows(rows)
+			},
+			wantErr: false,
+		},
+		{
+			name: "DB error",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				request: CreatePersonRequest{
+					Name:      "test name",
+					Email:     "test email",
+					CreatedBy: "test user",
+				},
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("INSERT INTO persons ").
+					WillReturnError(errors.New("test error"))
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock, _ := sqlmock.New()
+			defer func(db *sql.DB) {
+				_ = db.Close()
+			}(db)
+			tt.prepare(mock)
+			tt.fields.db = sqlx.NewDb(db, "sqlmock")
+			p := &PersonRepositoryImpl{
+				db: tt.fields.db,
+			}
+			_, err := p.Create(tt.args.request)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PersonRepositoryImpl.Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestPersonRepositoryImpl_DeleteByUuid(t *testing.T) {
+	type fields struct {
+		db *sqlx.DB
+	}
+	type args struct {
+		uuid uuid.UUID
+	}
+	testUuid, _ := uuid.Parse("2b1b425e-dee2-4227-8d94-f470a0ce0cd0")
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		prepare func(mock sqlmock.Sqlmock)
+	}{
+		{
+			name: "Success",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				uuid: testUuid,
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec("DELETE FROM persons WHERE alt_id = \\?").
+					WithArgs("2b1b425e-dee2-4227-8d94-f470a0ce0cd0").WillReturnResult(sqlmock.NewResult(1, 1))
+			},
+			wantErr: false,
+		},
+		{
+			name: "DB error",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				uuid: testUuid,
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec("DELETE FROM persons WHERE alt_id = \\?").
+					WithArgs("2b1b425e-dee2-4227-8d94-f470a0ce0cd0").WillReturnResult(sqlmock.NewResult(0, 0))
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock, _ := sqlmock.New()
+			defer func(db *sql.DB) {
+				_ = db.Close()
+			}(db)
+			tt.prepare(mock)
+			tt.fields.db = sqlx.NewDb(db, "sqlmock")
+			p := &PersonRepositoryImpl{
+				db: tt.fields.db,
+			}
+			results, _ := p.DeleteByUuid(tt.args.uuid)
+			if tt.wantErr && results.Deleted != false {
+				t.Errorf("PersonRepositoryImpl.DeleteByUuid() = %v, want %v", results.Deleted, true)
+			}
+		})
+	}
+}
+
+func TestPersonRepositoryImpl_Update(t *testing.T) {
+	type fields struct {
+		db *sqlx.DB
+	}
+	type args struct {
+		request UpdatePersonRequest
+	}
+	testUuid, _ := uuid.Parse("2b1b425e-dee2-4227-8d94-f470a0ce0cd0")
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		prepare func(mock sqlmock.Sqlmock)
+	}{
+		{
+			name: "Success",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				request: UpdatePersonRequest{
+					Id:           testUuid,
+					Name:         "test name",
+					Email:        "test email",
+					LastChangeBy: "test user",
+				},
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id", "name"}).
+					AddRow(1, "test name")
+				mock.ExpectQuery("UPDATE persons SET name = \\?, email = \\?, last_change_by = \\? WHERE alt_id = \\?").
+					WillReturnRows(rows)
+			},
+			wantErr: false,
+		},
+		{
+			name: "DB error",
+			fields: fields{
+				db: nil,
+			},
+			args: args{
+				request: UpdatePersonRequest{
+					Id:           testUuid,
+					Name:         "test name",
+					Email:        "test email",
+					LastChangeBy: "test user",
+				},
+			},
+			prepare: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("UPDATE persons SET name = \\?, email = \\?, last_change_by = \\? WHERE alt_id = \\?").
+					WillReturnError(errors.New("test error"))
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock, _ := sqlmock.New()
+			defer func(db *sql.DB) {
+				_ = db.Close()
+			}(db)
+			tt.prepare(mock)
+			tt.fields.db = sqlx.NewDb(db, "sqlmock")
+			p := &PersonRepositoryImpl{
+				db: tt.fields.db,
+			}
+			_, err := p.Update(tt.args.request)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PersonRepositoryImpl.Update() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestNewPersonRepository(t *testing.T) {
+	type args struct {
+		db *sqlx.DB
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Success",
+			args: args{
+				db: nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_ = NewPersonRepository(tt.args.db)
 		})
 	}
 }
