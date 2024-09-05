652b33c0ae0d8b31f8abe1c040045c17aa5ad2ce
terry <terry@xymox.dev>
2024-09-04 23:17:35 -0700

    Refactor SQL queries into constants
    Replaced inline SQL queries with constants for CREATE and UPDATE operations. This improves code readability and maintainability by centralizing query definitions in the repository.

--- item/repository.go
+++ item/repository.go
@@ -34,6 +34,8 @@ type UpdateItemRequest struct {
 }
 
 const (
+	CREATE_STATEMENT              = "INSERT INTO items (name, description, unit_price, created_by, last_changed_by) VALUES ($1, $2, $3, $4, $4) returning *"
+	UPDATE_STATEMENT              = "UPDATE items SET name = $1, description = $2, unit_price = $3, last_changed_by = $4 WHERE alt_id = $5 returning *"
 	GET_BY_ID_QUERY               = "SELECT * FROM items WHERE alt_id = $1"
 	GET_ALL_QUERY                 = "SELECT * FROM items"
 	GET_ALL_QUERY_WITH_PAGINATION = "SELECT * FROM items WHERE id > $1 OFFSET $2"
@@ -60,13 +62,13 @@ func NewItemRepository(db *sqlx.DB) *ItemRepositoryImpl {
 
 func (r *ItemRepositoryImpl) CreateItem(request CreateItemRequest) (ItemRow, error) {
 	var item ItemRow
-	err := r.db.Get(&item, `INSERT INTO items (name, description, unit_price, created_by, last_changed_by) VALUES ($1, $2, $3, $4, $4) returning *`, request.Name, request.Description, request.UnitPrice, request.CreatedBy)
+	err := r.db.Get(&item, CREATE_STATEMENT, request.Name, request.Description, request.UnitPrice, request.CreatedBy)
 	return item, err
 }
 
 func (r *ItemRepositoryImpl) UpdateItem(request UpdateItemRequest) (ItemRow, error) {
 	var item ItemRow
-	err := r.db.Get(&item, `UPDATE items SET name = $1, description = $2, unit_price = $3, last_changed_by = $4 WHERE alt_id = $5 returning *`, request.Name, request.Description, request.UnitPrice, request.LastChangedBy, request.Id)
+	err := r.db.Get(&item, UPDATE_STATEMENT, request.Name, request.Description, request.UnitPrice, request.LastChangedBy, request.Id)
 	return item, err
 }
 
