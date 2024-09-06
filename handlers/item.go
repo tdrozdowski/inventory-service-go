e431a5e7cdbc3456d1235e3b40ce034bd4202b2b
terry <terry@xymox.dev>
2024-09-05 21:47:19 -0700

    Add CreateItem API and tests
    Implemented a new POST endpoint for creating items in the ItemRoutes handler. Added related integration tests in item_test.go and a sample HTTP client script in item_api.http for testing the new functionality.

--- handlers/item.go
+++ handlers/item.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	"github.com/labstack/echo/v4"
 	"inventory-service-go/context"
+	"inventory-service-go/item"
 	"net/http"
 )
 
 func ItemRoutes(p *echo.Group, appContext context.ApplicationContext) {
-	p.GET("", AllItems(appContext))
+	p.GET("/items", AllItems(appContext))
+	p.POST("/items", CreateItem(appContext))
 }
 
 func AllItems(appContext context.ApplicationContext) func(c echo.Context) error {
@@ -19,5 +21,21 @@ func AllItems(appContext context.ApplicationContext) func(c echo.Context) error 
 			return c.JSON(http.StatusInternalServerError, err)
 		}
 		return c.JSON(http.StatusOK, items)
+	}
+}
+
+func CreateItem(appContext context.ApplicationContext) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		var createItemRequest item.CreateItemRequest
+		err := c.Bind(&createItemRequest)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		itemService := appContext.ItemService()
+		results, err := itemService.CreateItem(createItemRequest)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+		return c.JSON(http.StatusOK, results)
 	}
 }
--- handlers/item_test.go
+++ handlers/item_test.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/labstack/echo/v4"
@@ -8,9 +10,11 @@ import (
 	"inventory-service-go/commons"
 	"inventory-service-go/context"
 	"inventory-service-go/item"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestHandlers_AllItems(t *testing.T) {
@@ -70,4 +74,72 @@ func TestHandlers_AllItems(t *testing.T) {
 		})
 	}
 
+}
+
+func TestHandlers_CreateItem(t *testing.T) {
+	now := time.RFC3339
+	tests := []struct {
+		name               string
+		createItemRequest  item.CreateItemRequest
+		expectedResults    item.Item
+		expectedStatusCode int
+	}{
+		{
+			name:              "OK with a valid request",
+			createItemRequest: item.CreateItemRequest{Name: "TV", Description: "TV Description", UnitPrice: 10.99, CreatedBy: "Unit Test"},
+			expectedResults: item.Item{Name: "TV", Description: "TV Description", UnitPrice: 10.99, AuditInfo: commons.AuditInfo{
+				CreatedBy:     "Unit Test",
+				CreatedAt:     now,
+				LastUpdate:    "Unit Test",
+				LastChangedBy: now,
+			}},
+			expectedStatusCode: http.StatusOK,
+		},
+		{
+			name:               "Fail with an invalid request",
+			createItemRequest:  item.CreateItemRequest{Name: "", Description: "", UnitPrice: -1},
+			expectedResults:    item.Item{},
+			expectedStatusCode: http.StatusBadRequest,
+		},
+		{
+			name:               "Fail with Internal Server Error",
+			createItemRequest:  item.CreateItemRequest{Name: "", Description: "", UnitPrice: -1},
+			expectedResults:    item.Item{},
+			expectedStatusCode: http.StatusInternalServerError,
+		},
+	}
+	controller := gomock.NewController(t)
+	for _, tt := range tests {
+		mockItemService := item.NewMockItemService(controller)
+		mockApplicationContext := context.MockApplicationContext(nil, mockItemService)
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.expectedStatusCode == http.StatusInternalServerError {
+				mockItemService.EXPECT().CreateItem(gomock.Any()).Return(nil, errors.New("error"))
+			} else if tt.expectedStatusCode == http.StatusOK {
+				mockItemService.EXPECT().CreateItem(tt.createItemRequest).Return(&tt.expectedResults, nil)
+			}
+			e := echo.New()
+			var req *http.Request
+			if tt.expectedStatusCode == http.StatusBadRequest {
+				invalidPayload := []byte(`not json`)
+				req = httptest.NewRequest(http.MethodPost, "/", bytes.NewReader(invalidPayload))
+			} else {
+				requestJson, err := json.Marshal(tt.createItemRequest)
+				if err != nil {
+					t.Errorf("CreateItem() error = %v", err)
+				}
+				req = httptest.NewRequest(http.MethodPost, "/", io.NopCloser(bytes.NewReader(requestJson)))
+			}
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			err := CreateItem(mockApplicationContext)(c)
+			if err != nil {
+				t.Errorf("CreateItem() error = %v, expectedStatusCode %v", err, tt.expectedStatusCode)
+			}
+			if rec.Result().StatusCode != tt.expectedStatusCode {
+				t.Errorf("CreateItem() = %v, expectedStatusCode %v", rec.Code, tt.expectedStatusCode)
+			}
+		})
+	}
 }
